84f806cb59c7fe17e75b20f78722ad42f9e75e82
Luc-Olsthoorn <lucols97@gmail.com>
2017-10-12 21:39:13 -0400

    cleaned up

--- js/angular/math.js
+++
@@ -1,5 +0,0 @@
-app.controller('MainController', ['$scope', function($scope) {
-  $scope.move =
-  $scope.shutterbugg =
-  $scope.gameboard = 
-}]);
--- php/add.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "shit";
-$precoded  = $_POST['precode'];
-var_dump( $precoded );
-
-                        require_once "php/db_connect.php";
-                        require_once "php/nombackend.php";
-                        echo AddAmount($db);
-                     ?>
